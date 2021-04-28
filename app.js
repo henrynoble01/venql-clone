910ca0d3261780e3b94a287565b2fa5fdf5c45de
henrynoble01 <henrynoble01@gmail.com>
2021-04-28 15:43:11 +0100

    feat: first commmit in branch

---
+++ app.js
@@ -0,0 +1,10 @@
+function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle('hidden')
+    document.getElementById(collapseID).classList.toggle('block')
+}
+
+
+// function rotateIcon(iconID) {
+//     document.getElementsById(iconID).classList.toggle('rotate-180')
+//     document.getElementsById(iconID).classList.toggle('rotate-0')
+// }
