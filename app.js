f6cfa9db553b7ec9e7cf54eb3c085c9a322be329
henrynoble01 <henrynoble01@gmail.com>
2021-05-03 09:36:53 +0100

    chore: monday morning chech-point

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburgerIcom = document.querySelector('.harmburger-icon');
 const toogleHam = document.querySelector('.toggle');
-const trialBtn = document.querySelector('#free-trial-btn')
+const trialBtn = document.querySelector('#free-trial-btn');
+const navColor = document.querySelector('.navbar')
 
 // console.log(trialBtn);
 
@@ -9,5 +10,6 @@ hamburgerIcom.addEventListener('click', function(){
     // console.log('hello');
     toogleHam.classList.toggle('nav-toggle')
     trialBtn.classList.toggle('free-trial-btn')
+    navColor.classList.toggle('nav-toggle')
     // console.log(trialBtn);
 });
