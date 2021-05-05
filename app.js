d28f07126b7d2cbf82890e6817c9c11632bee635
henrynoble01 <henrynoble01@gmail.com>
2021-05-05 15:54:18 +0100

    feat: finished the fixed nav on scroll

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const hamburgerIcom = document.querySelector('.harmburger-icon');
 const toogleHam = document.querySelector('.toggle');
 const trialBtn = document.querySelector('#free-trial-btn');
-const navColor = document.querySelector('.navbar')
+const navColor = document.querySelector('.navbar');
+const stickyNav = document.querySelector('.section-3');
+const sticky = stickyNav.offsetTop;
+
+// console.log(stickyNav.offsetTop);
 
 // console.log(trialBtn);
 
@@ -12,4 +16,26 @@ hamburgerIcom.addEventListener('click', function(){
     trialBtn.classList.toggle('free-trial-btn')
     navColor.classList.toggle('nav-toggle')
     // console.log(trialBtn);
+});
+
+
+
+
+// window.onscroll = function() {myFunction()};
+// function myFunction() {
+//     if (window.pageYOffset >= sticky){
+//         stickyNav.classList.add('sticky');
+//         console.log(stickyNav.offsetTop);
+//     } else /* if (window.pageYOffset >  )*/ {
+//         stickyNav.classList.remove('sticky');
+//     }
+// }
+
+window.addEventListener('scroll', ()=> {
+    if (window.pageYOffset >= sticky){
+        stickyNav.classList.add('sticky');
+        // console.log(stickyNav.offsetTop);
+    } else /* if (window.pageYOffset >  )*/ {
+        stickyNav.classList.remove('sticky');
+    }
 });
